8a31941cff7c80f01844c58f9bf05af4b943c529
Phil Tipping <philtipping3110@gmail.com>
2024-03-27 14:09:50 +0000

    Add displayErrors function

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,6 +39,7 @@ async function postForm(e) {
     if (response.ok) {
         displayErrors(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 
@@ -76,6 +77,7 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 
@@ -91,5 +93,19 @@ function displayStatus(data) {
     document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
 
+}
+
+function displayException(data) {
+
+    let heading = `An Exception Occured`;
+
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error number: <strong>${data.error_no}</strong></div>`;
+    results += `<div>Error text: <strong>${data.error}</strong></div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    
+    resultsModal.show();
 }
 
